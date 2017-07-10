d3cbb76f5f67ea5047137d5b5485e2fc2b175b7a
Nate Geslin <teamtomkins23@gmail.com>
2017-07-09 21:01:43 -0500

    removes Tabs from FixRoot

--- src/assets/script/client/components/Fix/FixRoot.js
+++ src/assets/script/client/components/Fix/FixRoot.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Tabs from '../layout/Tabs/Tabs';
 import FixCreate from './FixCreate';
 import FixImport from './FixImport';
 import FixList from './FixList';
@@ -9,20 +8,11 @@ const FixRoot = (props) => {
     return (
         <div>
             <h2 className="hdg">Fix</h2>
-            <Tabs>
-                <Tabs.Tab title="List">
-                    <FixList fixList={ props.fixList }
-                        onRemoveFix={ props.onRemoveFix } />
-                </Tabs.Tab>
-                <Tabs.Tab title="Create">
-                    <FixCreate fix={ props.fix }
-                        onSaveFix={ props.onSaveFix } />
-                </Tabs.Tab>
-                <Tabs.Tab title="Import">
-                    <FixImport onImportFixList={ props.onImportFixList } />
-                </Tabs.Tab>
-            </Tabs>
-
+                <FixImport onImportFixList={ props.onImportFixList } />
+                <FixCreate fix={ props.fix }
+                    onSaveFix={ props.onSaveFix } />
+                <FixList fixList={ props.fixList }
+                    onRemoveFix={ props.onRemoveFix } />
         </div>
     );
 };
