c367a529a7903a79c3444037394ab535e8265e19
Nate Geslin <teamtomkins23@gmail.com>
2017-07-17 21:03:46 -0500

    adds placeholder text for remaining sections

--- src/assets/script/client/components/layout/Nav/Nav.js
+++ src/assets/script/client/components/layout/Nav/Nav.js
@@ -2,27 +2,34 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Nav = function() {
-    // render() {
-        return (
-            <div className="nav">
-                <ul className="hlist">
-                    <li>
-                        <Link to="/">Airport</Link>
-                    </li>
-                    <li>
-                        <Link to="/fixes">Fixes</Link>
-                    </li>
-                    <li>
-                        <Link to="/runways">Runways</Link>
-                    </li>
-                    <li>
-                        <Link to="/procedures">Procedures</Link>
-                    </li>
-                </ul>
-            </div>
-        );
-    // }
-}
+    return (
+        <div className="nav">
+            <ul className="hlist">
+                <li>
+                    <Link to="/">Airport</Link>
+                </li>
+                <li>
+                    <Link to="/fixes">Fixes</Link>
+                </li>
+                <li>
+                    <Link to="/runways">Runways</Link>
+                </li>
+                <li>
+                    <Link to="/procedures">Procedures</Link>
+                </li>
+                <li>
+                    Spawn Patterns
+                </li>
+                <li>
+                    Airspace
+                </li>
+                <li>
+                    Restricted Areas
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 Nav.displayName = 'Nav';
 
